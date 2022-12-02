8d1574f8bbd9dd42459bc5895b2a65d6caa4c219
Ron Matthew Alunan <ron.alunan@usjr.edu.ph>
2022-12-02 19:16:01 +0800

    added scan button widget

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 
-class ScanButton extends StatelessWidget {
-  const ScanButton({super.key});
+class ScanButton extends StatefulWidget {
+  const ScanButton({super.key, required this.label, required this.icon, required this.onPress});
+
+  final String label;
+  final Widget icon;
+  final Function() onPress;
 
   @override
+  State<ScanButton> createState() => _ScanButtonState();
+}
+
+class _ScanButtonState extends State<ScanButton> {
+  @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(100),
+          bottomLeft: Radius.circular(100)
+        ),
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      height: 120.0,
+      padding: const EdgeInsets.all(8.0),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
+            height: 100.0,
+            width: 100.0,
             decoration: const BoxDecoration(
-              shape: BoxShape.circle
+              shape: BoxShape.circle,
+              color: Color(0xffffffff),
             ),
-            child: Icon(
-              Icons.audiotrack,
-              color: Colors.green,
-              size: 30.0,
-            ),
+            child: widget.icon,
           ),
-          const Text('hell'),
+          Text(
+            style: Theme.of(context).textTheme.headline1?.merge(const TextStyle(
+              color: Color(0xffffffff),
+            )),
+            widget.label
+          ),
         ],
       )
     );
