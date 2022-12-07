f89d11c75d3d1b508a5ebf7ada4701d0e4d6d212
Aloysius Matthew A. Beronque <aloysius.beronue@usjr.edu.ph>
2022-12-07 13:10:12 +0800

    added ocr feature

--- lib/screens/scan_tab.dart
+++ lib/screens/scan_tab.dart
@@ -2,22 +2,31 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:quizpix/screens/scan_confirmation.dart';
 import 'package:quizpix/widgets/scan_button.dart';
 
-class ScanTab extends StatelessWidget {
+import 'dart:io';
+
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+main() async {
+  runApp(const MaterialApp(home: ScanTab()));
+}
+
+class ScanTab extends StatefulWidget {
   const ScanTab({super.key});
 
-  Future scanCamera() async {
-    try {
-      final XFile? image =
-          await ImagePicker().pickImage(source: ImageSource.camera);
-      if (image == null) {
-        return;
-      }
-    } on PlatformException {
-      return;
-    }
-  }
+  @override
+  _ScanTabState createState() => _ScanTabState();
+}
+
+class _ScanTabState extends State<ScanTab> {
+  String _text = '';
+  late PickedFile _image;
+  final picker = ImagePicker();
+  bool _load = false;
 
   @override
   Widget build(BuildContext context) {
@@ -82,14 +91,66 @@ class ScanTab extends StatelessWidget {
                     color: Color(0xfff69036),
                   ),
                   onPress: () {
-                    scanCamera();
+                    getImage();
                   },
                   isLeft: false),
               const Spacer(flex: 1),
+              _load == true
+                  ? Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      child: _image != null
+                          ? Image.file(
+                              File(_image.path),
+                              fit: BoxFit.fitWidth,
+                            )
+                          : Container(),
+                    )
+                  : Container()
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future scanText() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+    final FirebaseVisionImage visionImage =
+        FirebaseVisionImage.fromFile(File(_image.path));
+    final TextRecognizer textRecognizer =
+        FirebaseVision.instance.textRecognizer();
+    final VisionText visionText =
+        await textRecognizer.processImage(visionImage);
+
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+        _text += line.text + '\n';
+      }
+    }
+
+    Navigator.of(context).pop();
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => ScanConfirmation(text: _text)));
+  }
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        _image = pickedFile;
+        _load = false;
+        scanText();
+      } else {
+        print('No image selected');
+      }
+    });
   }
 }
